2e73ffe4b7354834baa0c28b27f496ff04385aa8
eloyEntony <kozyar.ant@gmail.com>
2020-03-03 14:25:43 +0200

    03.03.20

---
+++ Class_Work/07. Out, static class/07. Out, static class/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Out__static_class
+{
+    static class Calculator
+    {
+        public static void Plus(int num1, int num2, out int sum/*, params int[] num*/)
+        {
+            sum = num1 + num2;
+            //sum = 0;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    sum += num[i];
+            //}
+        }
+
+        public static void Minus(int num1, int num2, out int res/*, params int[] num*/)
+        {
+            res = num1 - num2;
+
+            //res = 0;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    res -= num[i];
+            //}
+        }
+
+        public static void Mnog(int num1, int num2, out int res/*, params int[] num*/)
+        {
+            res = num1 * num2;
+            //res = 0;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    res *= num[i];
+            //}
+        }
+
+        public static void Division(int num1, int num2, out int res/*, params int[] num*/)
+        {
+            res = num1 / num2;
+            //res = 0;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    res /= num[i];
+            //}
+        }
+
+    }
+}
---
+++ Class_Work/07. Out, static class/07. Out, static class/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Out__static_class
+{
+    class Program
+    {
+        //enum Operation
+        //{
+        //    add = 1,
+        //    minus,
+        //    mnog,
+        //    division,
+        //}
+        static void Main(string[] args)
+        {
+            //Calcucator
+          
+
+            //Calculator.Minus(out int res, 15, 55, 5);
+            //Console.WriteLine(res);
+
+            Console.WriteLine("Enter first num : ");
+            int num1 = int.Parse(Console.ReadLine());           
+            Console.WriteLine("Enter operation [Plus] [Minu] [Mnog] [Divi] : ");
+            //int operation = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            Console.WriteLine("Enter second num : ");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            //bool isParsed = int.TryParse(num1, out int number);
+            //if (isParsed){
+            //    Console.WriteLine(number);
+            //}
+            //else{
+            //    Console.WriteLine("Faled to parse");
+            //}
+
+
+            switch (operation)
+            {
+                case "+":
+                    Calculator.Plus(num1, num2, out int res);
+                    Console.WriteLine($"RESULT : {res}");
+                    break;
+                case "-":
+                    Calculator.Minus(num1, num2, out res);
+                    Console.WriteLine($"RESULT : {res}");
+                    break;
+                case "*":
+                    Calculator.Mnog(num1, num2, out res);
+                    Console.WriteLine($"RESULT : {res}");
+                    break;
+                case "/":
+                    if (num2 == 0){
+                        Console.WriteLine("Num2 = 0 -- Bad");
+                    }
+                    else{
+                        Calculator.Division(num1, num2, out res);
+                        Console.WriteLine($"RESULT : {res}");
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
