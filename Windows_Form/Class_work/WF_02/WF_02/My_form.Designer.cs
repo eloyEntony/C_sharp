2870ad39f290f8e424c096840df4ae2b58b1eecd
eloyEntony <kozyar.ant@gmail.com>
2020-04-07 16:01:38 +0300

    04.07.20

---
+++ Windows_Form/Class_work/WF_02/WF_02/My_form.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WF_02
+{
+    partial class My_form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(My_form));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 84);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(303, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 84);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl_time.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_time.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_time.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lbl_time.Location = new System.Drawing.Point(0, 0);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(52, 48);
+            this.lbl_time.TabIndex = 2;
+            this.lbl_time.Text = "0 s";
+            this.lbl_time.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // My_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(559, 450);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "My_form";
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Windows_Form/Class_work/WF_02/WF_02/My_form.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WF_02
+{
+    public partial class My_form : Form
+    {
+        public My_form()
+        {
+            InitializeComponent();
+        }
+
+        int i = 0;        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbl_time.Text = ++i + " s";
+            int max_withs = this.ClientSize.Width;
+            int max_heights = this.ClientSize.Height;
+            Point point = lbl_time.Location;
+
+            if (point.X < (max_withs - point.X / 9) && point.Y == 0)
+                lbl_time.Location = new Point(point.X += 10, point.Y);
+
+            else if (point.X > (max_withs - point.X / 9) && point.Y < (max_heights - point.Y / 1.6))
+                lbl_time.Location = new Point(point.X, point.Y += 10);
+
+            else if (point.Y >= (max_heights - point.Y / 1.6))
+                lbl_time.Location = new Point(point.X -= 10, point.Y);
+
+            if (point.X <= 0)
+                lbl_time.Location = new Point(point.X, point.Y -= 10);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            MessageBox.Show("Пройшло : " + lbl_time.Text, "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ((sender as Button).Parent as Form).Close();
+        }
+    }
+}
